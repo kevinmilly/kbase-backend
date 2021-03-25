4df4c36d1dd7d0308fe0c65f393772af25905761
accountantKev <50839401+accountantKev@users.noreply.github.com>
2021-03-24 22:57:16 -0400

    changes to model and services

--- models/concepts.js
+++ models/concepts.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const conceptSchema = mongoose.Schema({
     title:{type: String, required:true},
-    resource:{type: String, required:true},
-    difficulty:{type: Number, default:0},
+    necessity:{type: Number, default:0},
     dependentConcepts:{type: [String], default:[]},
     relatedNotes:{type: [String], default:[]},
     lastRecalled:{type: String, default: new Date().toDateString()},
-    status:{type:Number, default:0},
+    level:{type:Number, default:0},
     completed:{type:Boolean, default:false},
+    tag:{type:String, default:false},
+    details: {type: String, required:false}
 
 
 })
 
-module.exports = mongoose.model('Concept', conceptSchema);
+module.exports = mongoose.model('Concept', conceptSchema);  
--- models/tasks.js
+++ models/tasks.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema({
     title:{type: String, required:true},
-    resource:{type: String, required:true},
     difficulty:{type: Number, default:0},
-    dependenttasks:{type: [String], default:[]},
     lastRecalled:{type: String, default: new Date().toDateString()},
     status:{type:Number, default:0},
     completed:{type:Boolean, default:false},
+    tag:{type:String, default:false},
 })
 
 module.exports = mongoose.model('task', taskSchema);
