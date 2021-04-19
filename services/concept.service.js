d15668ac30576a92bb923410261a14079a07b2e4
kevinmilly <kevinmilly@gmail.com>
2021-04-18 21:47:54 -0400

    made changes to the reactivity of CRUD operations

--- services/concept.service.js
+++ services/concept.service.js
@@ -1,24 +1,21 @@
 const Concept = require('../models/concepts');
 
 exports.addConcept = (req,res,next) => {
-    console.log("add concept on the backend");
     const concept = new Concept({
       title:req.body.title,
-      resource:req.body.resource,
-      difficulty: req.body.difficulty,
-      status: req.body.status,
-      notes: req.body.notes
+      necessity: req.body.necessity, 
+      level: req.body.level,
+      tag: req.body.tag
     })
+    console.dir(concept);
     concept.save();
-    res.status(201).json({
-        message: 'Concept added successfully!'
-    });
+    res.status(201).json(concept);
 }
 
 exports.getConcepts =  (req,res,next)=> {
     Concept.find()
     .then((documents) => {
-        // console.log(documents);
+        console.log(documents);
 
         res.status(200).json({
             message:"Concept has been fetched",
@@ -32,12 +29,13 @@ exports.editConcept = (req,res,next) => {
     const concept = new Concept({
         _id: req.body.id,
         title:req.body.title,
-        difficulty: req.body.difficulty,
-        status: req.body.status,
+        necessity: req.body.necessity,
+        level: req.body.level,
         notes: req.body.notes,
         dependentConcepts: req.body.dependentConcepts,
         relatedNotes: req.body.relatedNotes,
         completed: req.body.completed,
+        tag: req.body.tag
 
       })
        Concept.updateOne({_id: concept.id}, concept, {upsert: true}, function(err, doc) {
@@ -50,7 +48,10 @@ exports.editConcept = (req,res,next) => {
 
 exports.deleteConcept = (req, res, next) => {
     console.log("in delete");
-    console.dir(req);
-    Concept.find({id:req.body.id}).remove().exec();
+    // console.dir(req);
+    Concept.deleteOne({id:req.body.id}, function (err) {
+        if(err) console.log(err);
+        console.log("Successful deletion");
+      });
     res.status(200).json({message: "Post deleted!"});
 }
