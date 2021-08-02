4b749b436936621f61da5398dab4efcf42ae7ba5
kevinmilly <kevinmilly@gmail.com>
2021-08-02 17:13:34 -0400

    fixed auth issues

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-    );y
+    );
     next();
 })
 
